580ffa372ab3474456931d3b8ce62ed84c7b4017
Lewis Murray <lewisdmurray00@gmail.com>
2023-10-10 00:05:22 +0100

    Started creating more on the hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,25 @@ import { ComputersCanvas } from './canvas';
 const Hero = () => {
   return (
     <section className='relative w-full h-screen mx-auto'>
-      Hero
+      <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
+        <div className='flex flex-col justify-center items-center mt-5'>
+          <div className='w-5 h-5 rounded-full bg-[#915eff]'/>
+          <div className='w-1 sm:h-80 h-40 violet-gradient'/>
+
+          <div>
+            <h1 className={`${styles.heroHeadText} text-white`}>
+              Hi, I'm <span className='text-[#915eff]'>Lewis Murray</span>
+            </h1>
+
+            <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+              I work within the digital forensic industry and am also a web developer.
+            </p>
+          </div>
+
+          <ComputersCanvas />
+
+        </div>
+      </div>
     </section>
   )
 }
