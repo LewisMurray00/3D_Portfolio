34b4cde6b308c3a5d29cab93cf2714f485ebd72e
Lewis Murray <lewisdmurray00@gmail.com>
2023-10-30 23:45:21 +0000

    Created the input form for the messages from the user

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -36,6 +36,49 @@ const Contact = () => {
       >
         <p className={styles.sectionSubText}>Get in touch</p>
         <h3 className={styles.sectionHeadText}>Contact.</h3>
+
+        <form
+          ref={formRef}
+          onSubmit={handleSubmit}
+          className='mt-12 flex flex-col gap-8'
+        >
+          <label className='flex flex-col'>
+            <span className='text-white font-medium mb-4'>Your name</span>
+            <input 
+              type='text'
+              name='name'
+              value={form.name}
+              onChange={handleChange}
+              placeholder='Enter your name'
+              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
+            />
+          </label>
+
+          <label className='flex flex-col'>
+            <span className='text-white font-medium mb-4'>Your Email</span>
+            <input 
+              type='email'
+              name='email'
+              value={form.email}
+              onChange={handleChange}
+              placeholder='Enter your email'
+              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
+            />
+          </label>
+
+          <label className='flex flex-col'>
+            <span className='text-white font-medium mb-4'>Your Message</span>
+            <textarea
+              rows="7"
+              name='message'
+              value={form.message}
+              onChange={handleChange}
+              placeholder='Enter your message'
+              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
+            />
+          </label>
+
+        </form>
       </motion.div>
 
     </div>
