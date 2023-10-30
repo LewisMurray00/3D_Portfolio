53e8ec6d5d14ee317f90cd40f764f5e057addb30
Lewis Murray <lewisdmurray00@gmail.com>
2023-10-30 23:23:42 +0000

    started the contact page - added the functions required

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import { useState, useRef } from 'react'; 
+import { motion } from 'framer-motion';
+import emailJS from '@emailjs/browser';
+
+import { styles } from '../style';
+import { EarthCanvas } from './canvas';
+import { SectionWrapper } from '../hoc';
+import { slideIn } from '../utils/motion';
 
 const Contact = () => {
+
+  const formRef = useRef();
+
+  const [form, setForm] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e) => {
+
+  };
+
+  const handleSubmit = (e) => {
+
+  };
+
   return (
-    <div>Contact</div>
+    <div>2:13:13</div>
   )
 }
 
